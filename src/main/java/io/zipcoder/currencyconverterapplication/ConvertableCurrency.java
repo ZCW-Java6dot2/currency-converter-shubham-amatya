bb855e36bc430f472bb226af851e779dbd93a347
shubham <shubham@zipcodes-MBP-5.fios-router.home>
2020-11-30 23:19:39 -0500

    Cleaned methods

--- src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
+++ src/main/java/io/zipcoder/currencyconverterapplication/ConvertableCurrency.java
@@ -3,7 +3,10 @@ package io.zipcoder.currencyconverterapplication;
 public interface ConvertableCurrency {
     default Double convert(CurrencyType currencyType) {
 
-        return currencyType.getRate()/getCurrencyType().getRate();
+        Double rateOfGivenCurrency = currencyType.getRate(); //rate of currency passed (base currency) as the argument
+        Double rateOfNewCurrency = CurrencyType.getTypeOfCurrency(this).getRate(); // rate of currency to be converted to (target currency)
+
+        return rateOfGivenCurrency/rateOfNewCurrency;
     }
     CurrencyType getCurrencyType();
 }
